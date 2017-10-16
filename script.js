2295c091cc8843a05daa95f1a119ba2998eb8d83
gprough <31858196+gprough@users.noreply.github.com>
2017-10-16 09:30:40 -0700

    edit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//this function responds with a fortune
+//when click the magic 8 ball image
 function shakeMagic8Ball(){
-    alert("viewer discretion is advised");
+    //alert("viewer discretion is advised");
+
+    //grab image by id
+    document.getElementById("magic8ball").src=".jpg"
+
+    //grab the results
+    document.getElementById("results").innerHTML="It's Decidely So!"
+
 }
