9bd99593683fe91cc399ec1952ba9d9afab399f4
gprough <31858196+gprough@users.noreply.github.com>
2017-10-16 09:34:30 -0700

    edit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function shakeMagic8Ball(){
     //alert("viewer discretion is advised");
 
     //grab image by id
-    document.getElementById("magic8ball").src=".jpg"
+    document.getElementById("magic8ball").src="magic8ball2.jpg"
 
     //grab the results
     document.getElementById("results").innerHTML="It's Decidely So!"
