697ee61474d5001f150532dc918aa640da0a64ed
gprough <31858196+gprough@users.noreply.github.com>
2017-10-23 08:31:41 -0700

    edit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//create a list of strings, to hold 8 ball responses
+var list = ["yes.",
+            "no!",
+            "Try again.",
+            "Concentrate and try again.",
+            "signs point to yes.",
+            "My Reply is no.",
+            "Don't count on it.",
+            "Reply hazy, try again."];
+
+
 //this function responds with a fortune
 //when click the magic 8 ball image
 function shakeMagic8Ball(){
@@ -7,6 +18,7 @@ function shakeMagic8Ball(){
     document.getElementById("magic8ball").src="magic8ball2.jpg"
 
     //grab the results
-    document.getElementById("results").innerHTML="It's Decidely So!"
+    document.getElementById("results").innerHTML=list[5];
 
+   //alert(list[5]);
 }
