5a45d31ba6bdfd90c445c7479519297d4fc8757f
gprough <31858196+gprough@users.noreply.github.com>
2017-10-30 09:40:01 -0700

    edit

--- tictactowscript.js
+++ tictactowscript.js
@@ -2,5 +2,14 @@ var character = 'x';
 
 //This function handles the onclick event
 function turn(location){
+
+    //switch between x and o
+    if(character == 'x'){
+        character = 'o';
+    }
+    else{
+        character = 'x';
+    }
+
     document.getElementById(location).innerHTML = character;
 }
