6ffeb3dbed8ee23137fed5fc70b80f1fa03f6c67
gprough <31858196+gprough@users.noreply.github.com>
2017-11-13 09:13:43 -0800

    edit

--- tictactowscript.js
+++ tictactowscript.js
@@ -16,8 +16,32 @@ function turn(location){
     else{
         character = 'x';
     }
-
+        //write character in box
         document.getElementById(location).innerHTML = character;
+        //chack for winner after move
+        checkwinner();
   }
 
+}
+
+//checks for winner
+function checkwinner(){
+
+  //grab HTML of each tic tac toe div (which holds x or o)
+  var r1c1 = document.getElementById("r1c1").innerHTML;
+  var r1c2 = document.getElementById("r1c2").innerHTML;
+  var r1c3 = document.getElementById("r1c3").innerHTML;
+  var r2c1 = document.getElementById("r2c1").innerHTML;
+  var r2c2 = document.getElementById("r2c2").innerHTML;
+  var r2c3 = document.getElementById("r2c3").innerHTML;
+  var r3c1 = document.getElementById("r3c1").innerHTML;
+  var r3c2 = document.getElementById("r3c2").innerHTML;
+  var r3c3 = document.getElementById("r3c3").innerHTML;
+
+  if(((r1c1 == r1c2) && (r1c1 == r1c3) && r1c1 != '') ||
+     ((r2c1 == r2c2) && (r2c1 == r2c3) && r2c1 != '') ||
+     ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') ||
+     ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') ){
+      alert("Winner!");
+  }
 }
