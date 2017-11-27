3dd56b5e4efdf1e66a2901838dd4e3212042c5f1
gprough <31858196+gprough@users.noreply.github.com>
2017-11-27 09:41:12 -0800

    new code

--- tictactowscript.js
+++ tictactowscript.js
@@ -1,10 +1,12 @@
 var character = 'o';
+var gameOver = false;
 
 //This function handles the onclick event
 function turn(location){
 
   //update box with character, if box is empty
-  if(document.getElementById(location).innerHTML == ""){
+  if(document.getElementById(location).innerHTML == "" &&
+gameOver == false){
 
     //update results div with feedback
     document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
@@ -43,10 +45,29 @@ function checkwinner(){
      ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') || //third row
      ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') || //first column
      ((r1c1 == r2c2) && (r1c1 == r3c3) && r1c1 != '')){  //left to right diagonal
-     
+
+       //game is over now, there's a winner
+       gameOver = true;
+
       alert("Winner!");
 
       //update results div
       document.getElementById("results").innerHTML = character + "wins!";
   }
+}
+
+//clear tic tac toe tictactoeboard
+function clearAll(){
+    document.getElementById("r1c1").innerHTML="";
+    document.getElementById("r1c2").innerHTML="";
+    document.getElementById("r1c3").innerHTML="";
+    document.getElementById("r2c1").innerHTML="";
+    document.getElementById("r2c2").innerHTML="";
+    document.getElementById("r2c3").innerHTML="";
+    document.getElementById("r3c1").innerHTML="";
+    document.getElementById("r3c2").innerHTML="";
+    document.getElementById("r3c3").innerHTML="";
+
+    //reset gameOver, so players can play again
+    gameOver = false;
 }
