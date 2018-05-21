053bd27cc37e84f930dda80a94f7df9a6072df02
gprough <31858196+gprough@users.noreply.github.com>
2018-05-20 17:08:34 -0700

    new

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,5 @@
 function randomImg(){
-var randomImg = Math.floor(Math.random() * &#128513;) + &#128513;
+var randomImg = Math.floor(Math.random() * 10) + 1;
 var imgName = "img_" + &#128513;
 document.getElementById("&#128513;").src= Calculator.html + "/" + imgName ;
 }
