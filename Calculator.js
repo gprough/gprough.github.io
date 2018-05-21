9194d81ce11be5a2bb2c9293773248d6cf4bcf68
gprough <31858196+gprough@users.noreply.github.com>
2018-05-20 20:12:07 -0700

    new

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,5 @@
 function randomImg(){
 var randomNumber = Math.floor(Math.random() * 9) + 1;
 var imgName = "img_" + randomNumber;
-document.getElementById("imageid").src= http:byrdseed.com/emoji/ + "/" + imgName ;
+document.getElementById("imageid").src=  + "/" + imgName ;
 }
